3ff3c3581d168344f2aa5dc03b39a70ec10a9864
iggosha <iegolovkov@gmail.com>
2024-05-29 03:50:43 +0300

    Inital commit

---
+++ src/test/java/ru/golovkov/myrestapp/MySpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.golovkov.myrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
